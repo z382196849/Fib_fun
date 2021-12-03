483d7ac7b8a19193c359f0db46819f92069e8772
z382196829 <1936851315@qq.com>
2021-12-03 22:58:59 +0800

    第十七次提交

---
+++ Fib_fun.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<windows.h>
+
+int Fib(int n)
+{
+    if(n <= 2)
+    {
+ 	return 1;
+    }
+    else
+    {
+	return Fib(n - 1) + Fib(n - 2);
+    }
+}
+
+int main()
+{ 
+    int n = 0;
+    int ret = 0;
+    printf("请输入：");
+    scanf("%d", &n);
+    ret = Fib(n);
+    printf("%d\n", ret);
+    system("pause");
+    return;
+}
